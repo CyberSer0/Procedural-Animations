cd13877b817b9970272aa9bc5fb18217b3b21952
Artur Szałyga <arturgames14@gmail.com>
2023-10-04 11:22:16 +0200

    Fixed leg rotation

---
+++ Scenes/CameraFollow.gd
@@ -0,0 +1,12 @@
+extends Camera3D
+
+@export var node_to_follow : Node
+
+var start_offset : Vector3
+
+func _ready():
+	start_offset = global_position
+
+func _physics_process(delta):
+	if node_to_follow != null:
+		global_position = node_to_follow.global_position + start_offset
--- Spider/Spider.gd
+++ Spider/Spider.gd
@@ -1,45 +1,45 @@
 extends CharacterBody3D
 
 @export var SPEED : float = 10.0
+@export var ROTATION_SPEED : float = 2.0
 
 @export var STEP_DISTANCE : float = 2.0
 
-var leg1_basepos : Vector3
-var leg2_basepos : Vector3
-var leg3_basepos : Vector3
-var leg4_basepos : Vector3
+var is_leg1_moving : bool = false
+var is_leg2_moving : bool = false
+var is_leg3_moving : bool = false
+var is_leg4_moving : bool = false
+
+var can_walk : bool = true
 
 func _ready():
-	init_base_values()
-
+	$Leg1Control.global_position = global_position + $Leg1Basepos.position
 
 func _physics_process(delta):
-		velocity = Vector3(Input.get_axis("move_left", "move_right"), 0, Input.get_axis("move_forward", "move_backwards")) * SPEED
-		move_and_slide()
-		control_legs()
-		print("Leg1: ", $Leg1Control.position, leg1_basepos, $Leg1Control.position.distance_to(leg1_basepos)) 
+	print($Leg1Control.position.distance_to($Leg1Basepos.position))
+	velocity = global_transform.basis.z * Input.get_axis("move_forward", "move_backwards") * SPEED
+	rotation_degrees += global_transform.basis.y * Input.get_axis("move_right", "move_left") * ROTATION_SPEED
+	move_and_slide()
+	control_legs()
+		
 
 func control_legs():	
-	if $Leg1Control.global_position.distance_to(global_position + leg1_basepos) > STEP_DISTANCE:
-		step($Leg1Control, leg1_basepos)
-#	elif abs(global_position.distance_to($Leg2Control.global_position) - global_position.distance_to($Leg2Control.global_position + leg2_basepos)) < 4:
-#		step($Leg2Control, leg2_basepos)
-#	if abs(global_position.distance_to($Leg3Control.global_position) - global_position.distance_to($Leg3Control.global_position + leg3_basepos)) < 4:
-#		step($Leg3Control, leg3_basepos)
-#	elif abs(global_position.distance_to($Leg4Control.global_position) - global_position.distance_to($Leg4Control.global_position + leg4_basepos)) < 4:
-#		step($Leg4Control, leg4_basepos)
+	if $Leg1Control.global_position.distance_to(global_position + $Leg1Basepos.position) > STEP_DISTANCE or $Leg3Control.global_position.distance_to(global_position + $Leg3Basepos.position) > STEP_DISTANCE:
+		step($Leg1Control, $Leg1Basepos.global_position, is_leg1_moving)
+		step($Leg3Control, $Leg3Basepos.global_position, is_leg3_moving)
+	if $Leg2Control.global_position.distance_to(global_position + $Leg2Basepos.position) > STEP_DISTANCE or $Leg4Control.global_position.distance_to(global_position + $Leg4Basepos.position) > STEP_DISTANCE: 
+		step($Leg2Control, $Leg2Basepos.global_position, is_leg2_moving)
+		step($Leg4Control, $Leg4Basepos.global_position, is_leg4_moving)
 
-func step(control : Node, added_value : Vector3):
-	var target = global_position + added_value
-	var half_way = (control.global_position + target) / 2
+func step(control : Node, target_value : Vector3, is_leg_moving : bool):
+	var half_way = (control.global_position + target_value) / 2
 	var t = get_tree().create_tween()
-	
+	print("Leg1: ", is_leg1_moving, "\t| Leg2: ", is_leg2_moving, "\t| Leg3: ", is_leg3_moving, "\t| Leg4: ", is_leg4_moving)
 	t.tween_property(control, "global_position", half_way + owner.basis.y / 2, 0.1)
-	t.tween_property(control, "global_position", target, 0.05)
-	
+	t.tween_property(control, "global_position", target_value, 0.1)
+	t.connect("finished", on_step_finished)
 
-func init_base_values():
-	leg1_basepos = $Leg1Control.position
-	leg2_basepos = $Leg2Control.position
-	leg3_basepos = $Leg3Control.position
-	leg4_basepos = $Leg4Control.position
+func on_step_finished(control : Node):
+	can_walk = true
+	print("can walk again")
+	
