258dcb8a5a522f5f5a1330cb269ac7b035692b81
CyberSer <arturgames14@gmail.com>
2023-10-20 02:18:28 +0200

    Working turning commit

--- Spider/Spider.gd
+++ Spider/Spider.gd
@@ -1,38 +1,60 @@
 extends CharacterBody3D
 
+signal front_collision_detected
+
 @export var SPEED : float = 10.0
 @export var ROTATION_SPEED : float = 2.0
 
 @export var STEP_DISTANCE : float = 1.5
 
-var is_leg1_moving : bool = false
-var is_leg2_moving : bool = false
-var is_leg3_moving : bool = false
-var is_leg4_moving : bool = false
+@export var MAIN_BODY : Node3D
 
+var can_walk : bool = false
+
+@onready var head_tweener : Tween = get_tree().create_tween()
+
+#func _ready():
+#	front_collision_detected.connect(on_head_collision_detected(Vector3.ZERO))
+
+func _input(event):
+	if Input.get_axis("move_forward", "move_backwards") or Input.get_axis("move_right", "move_left") or Input.is_action_just_released("move_forward") or Input.is_action_just_released("move_backwards") or Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right"):
+		control_legs()
+	
+#	if $Detectors/F_RayHead.is_colliding and Input.get_axis("move_forward", "move_backwards") > 0:
+#		emit_sgnal("front_collision_detected", -$Detectors/F_RayHead.get_collision_normal)
 
 
 func _physics_process(delta):
-	print($Leg1Control.global_position.distance_to(global_position + $Leg1Basepos.position))
-	velocity = global_transform.basis.z * Input.get_axis("move_forward", "move_backwards") * SPEED
-	rotation_degrees += global_transform.basis.y * Input.get_axis("move_right", "move_left") * ROTATION_SPEED
+	velocity = MAIN_BODY.global_transform.basis.z * Input.get_axis("move_forward", "move_backwards") * SPEED
+	rotation_degrees += MAIN_BODY.global_transform.basis.y * Input.get_axis("move_right", "move_left") * ROTATION_SPEED
 	move_and_slide()
-	control_legs()
 	
 
 func control_legs():	
-	if $Leg1Control.global_position.distance_to(global_position + $Leg1Basepos.position) > STEP_DISTANCE or $Leg3Control.global_position.distance_to(global_position + $Leg3Basepos.position) > STEP_DISTANCE:
-		step($Leg1Control, $Leg1Basepos.global_position, is_leg1_moving)
-		step($Leg3Control, $Leg3Basepos.global_position, is_leg3_moving)
-	if $Leg2Control.global_position.distance_to(global_position + $Leg2Basepos.position) > STEP_DISTANCE or $Leg4Control.global_position.distance_to(global_position + $Leg4Basepos.position) > STEP_DISTANCE: 
-		step($Leg2Control, $Leg2Basepos.global_position, is_leg2_moving)
-		step($Leg4Control, $Leg4Basepos.global_position, is_leg4_moving)
+	if ($Leg1Control.global_position.distance_to(global_position + $Leg1Basepos.position) > STEP_DISTANCE or $Leg3Control.global_position.distance_to(global_position + $Leg3Basepos.position) > STEP_DISTANCE) and can_walk:
+		can_walk = false
+		step($Leg1Control, $Leg1Basepos.global_position)
+		step($Leg3Control, $Leg3Basepos.global_position)
+		step($Leg2Control, $Leg2Basepos.global_position)
+		step($Leg4Control, $Leg4Basepos.global_position)
+	elif $Leg2Control.global_position.distance_to(global_position + $Leg2Basepos.position) > STEP_DISTANCE or $Leg4Control.global_position.distance_to(global_position + $Leg4Basepos.position) > STEP_DISTANCE: 
+		can_walk = false
+		step($Leg2Control, $Leg2Basepos.global_position)
+		step($Leg4Control, $Leg4Basepos.global_position)
+		step($Leg1Control, $Leg1Basepos.global_position)
+		step($Leg3Control, $Leg3Basepos.global_position)
 
-func step(control : Node, target_value : Vector3, is_leg_moving : bool):
+func step(control : Node, target_value : Vector3):
 	var half_way = (control.global_position + target_value) / 2
 	var t = get_tree().create_tween()
-	print("Leg1: ", is_leg1_moving, "\t| Leg2: ", is_leg2_moving, "\t| Leg3: ", is_leg3_moving, "\t| Leg4: ", is_leg4_moving)
 	t.tween_property(control, "global_position", half_way + owner.basis.y / 2, 0.1)
 	t.tween_property(control, "global_position", target_value, 0.1)
-
+	t.tween_callback(set_can_walk)
 	
+
+func set_can_walk():
+	can_walk = true
+	
+
+#func on_head_collision_detected(new_facing_direction : Vector3):
+#	head_tweener.tween_property(MAIN_BODY, "rotation", rotation, new_facing_direction.rotated(MAIN_BODY.global_transform.basis.x, 90))
